8ce64f2be80f741d31230f0db4bf20c24e37f978
João Correia <joaocorreiaprof@gmail.com>
2024-07-28 16:32:34 +0100

    Added more functions to the linked list
    find(), removeAt(), contians(), pop()

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ ll.insertFirst(300);
 ll.insertFirst(200);
 ll.insertFirst(100);
 ll.insertLast(400);
+ll.insertLast(500);
+ll.removeAt(4);
+ll.pop();
+ll.insertAt(900, 1);
+
 console.log("Size of linked list:", ll.getSize());
 console.log("Head of linked list:", ll.getHead());
 console.log("Tail of the linked list:", ll.getTail());
 console.log(ll.getAt(2));
+console.log(ll.contains(1000));
+console.log(ll.find(300));
 console.log(JSON.stringify(ll));
--- src/modules/LinkedList.js
+++ src/modules/LinkedList.js
@@ -25,6 +25,34 @@ export default class LinkedList {
     }
     this.size++;
   }
+  insertAt(data, index) {
+    //If index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    //If first index
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+    const node = new Node(data);
+    let current, previous;
+
+    //Set current to first
+    current = this.head;
+    let count = 0;
+
+    while (count < index) {
+      previous = current; //Noded before the index
+      count++;
+      current = current.next; //Node after the index
+    }
+
+    node.next = current;
+    previous.next = node;
+
+    this.size++;
+  }
 
   getSize() {
     let count = 0;
@@ -65,5 +93,75 @@ export default class LinkedList {
     }
 
     return current.data;
+  }
+  removeAt(index) {
+    if (index < 0 && index > this.size) {
+      return;
+    }
+
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+
+      previous.next = current.next;
+    }
+    this.size--;
+  }
+
+  pop() {
+    if (!this.head) {
+      return;
+    }
+
+    if (!this.head.next) {
+      this.head = null;
+      this.size--;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next.next) {
+      current = current.next;
+    }
+
+    current.next = null;
+    this.size--;
+  }
+  contains(value) {
+    if (this.head === null) {
+      return false;
+    }
+    let current = this.head;
+    while (current) {
+      if (current.data === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  find(value) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+      if (current.data === value) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+
+    return null;
   }
 }
